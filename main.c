4e79805eec4e9fb77ddb2e91529515b57154eb4c
Unknown <19gures97@gmail.com>
2018-04-24 13:56:31 +0200

    Już działa :)

--- main.c
+++ main.c
@@ -7,12 +7,14 @@
 #include <unistd.h>
 #include <syslog.h>
 #include "command_list.h"
+#include "daemon_functions.h"
 
 int main(int argc, char* argv[]){
 	pid_t pid, sid;
-	int zadania_fd, logi_fd;
+	int taskfile_fd, outfile_fd;
+	char komenda[] = "eject";
 
-	pid = fork();
+	/*pid = fork();
 	if(pid < 0){
 		exit(EXIT_FAILURE);
 	}
@@ -38,22 +40,27 @@ int main(int argc, char* argv[]){
         exit(1);
 	}
 
+	printf("%s\n", argv[1]);
+	taskfile_fd = open(argv[1], O_RDONLY);
+    	if(taskfile_fd == -1) {
+        	perror(argv[1]);
+        	exit(2);
+    	}
+	printf("%s\n", argv[2]);
+	outfile_fd = open(argv[2], O_WRONLY);
+    	if(outfile_fd == -1) {
+        	perror(argv[2]);
+        	exit(2);
+    	}
 
-	int taskfile_fd = open(argv[1], O_RDONLY);
-    if(taskfile_fd == -1) {
-        perror(argv[1]);
-        exit(2);
-    }
-
-    //close(STDIN_FILENO);
+	//close(STDIN_FILENO);
 	//close(STDOUT_FILENO);
 	//close(STDERR_FILENO);
-
-	CommandList* root = NULL;
-
-	createCommandList(root, taskfile_fd);	
+	
 	printf("Succes\n");
-
+	printf("Argv[0] tego programu: %s\n", argv[0]);
+	runCommand(komenda,0,outfile_fd);
+	printf("Lecimy temacik\n");
 /*	
 
 	while(1){
@@ -62,6 +69,7 @@ int main(int argc, char* argv[]){
 		sleep(30);
 	}
 */
-    close(taskfile_fd);
+	close(taskfile_fd);
+	close(outfile_fd);
 	exit(EXIT_SUCCESS);
 }
